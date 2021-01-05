d5a35e19490b72d36213f4ceb86e8f3e3d3b3813
BublG <vasyapupkin123456123456@mail.ru>
2021-01-05 16:46:24 +0300

    correct_for_react-script_at_WebSecConf

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,16 +1,7 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-class App extends React.Component {
-
-    render() {
-        return (
-            <h1>Hello gandon</h1>
-        )
-    }
-}
-
 ReactDOM.render(
-    <App />,
-    document.getElementById('react')
-)
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+);
